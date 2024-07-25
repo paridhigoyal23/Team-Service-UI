11dca905549847eea55f291b1a894a3ffdb0dce6
AnjaliBind12345 <anbind3724@gmail.com>
2024-07-25 10:35:54 +0530

    Deleted the Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
-
 import DashboardPage from './pages/Dashboard.jsx';
 import TeamMembersPage from './pages/TeamMembers.jsx';
 import WFOPage from './pages/WFO.jsx';
@@ -8,16 +7,14 @@ import RootLayout from './pages/Root.jsx';
 import './App.css';
 
 const router=createBrowserRouter([
-  {
-    path:'/Team-Service-UI/', 
-    element:<RootLayout/>,
-    children:[
-      {path:'', element:<DashboardPage/>},
-      {path:'teammembers', element:<TeamMembersPage/>},
-      {path:'wfo', element:<WFOPage/>},
-      {path:'training', element:<TrainingPage/>}
-    ],
-  },
+   {path:'/Team-Service-UI/', element:<RootLayout/>,
+      children:[
+          {path:'', element:<DashboardPage/>},
+          {path:'teammembers', element:<TeamMembersPage/>},
+          {path:'wfo', element:<WFOPage/>},
+          {path:'training', element:<TrainingPage/>}
+        ],
+      },
 ]);
 
 function App() {
