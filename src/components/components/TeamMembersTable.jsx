63f6c1271ab0ef1d321c394487a7965977120d4b
umanghere <umangbansal1st@gmail.com>
2024-08-01 22:22:34 +0530

    Updated TeamMembersTable

--- src/components/components/TeamMembersTable.jsx
+++ src/components/components/TeamMembersTable.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import UserForm from '../../Elements/TrainingTable/AddIcon/UserFrom';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
